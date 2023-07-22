b23d50860d9980e22b7abe4e6f6f2f2a6d990be9
Max <maxhannan@quenelle.app>
2023-07-22 01:36:25 -0500

    update

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex  ">
-      <Link href="/app/dashboard">Dashboard</Link>
+      <Link href="/app/dashboard">Dashboard 2</Link>
     </main>
   );
 }
