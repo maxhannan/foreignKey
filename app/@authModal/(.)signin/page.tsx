ea9ad5a10a7ca88d4db42cde1ed3002e65811b66
Max <maxhannan@quenelle.app>
2023-07-27 19:29:51 -0500

    spacing

--- app/@authModal/(.)signin/page.tsx
+++ app/@authModal/(.)signin/page.tsx
@@ -6,7 +6,7 @@ import { FC } from "react";
 const page: FC = () => {
   return (
     <div className="fixed inset-0 bg-zinc-900/20 z-10">
-      <div className="px-4 container mt-16 sm:mt-0 sm:px-0  flex items-center h-full max-w-[450px] w-full mx-auto">
+      <div className="px-4 container mt-8 sm:mt-0 sm:px-0  flex items-center h-full max-w-[450px] w-full mx-auto">
         <div className="relative bg-background dark:border  dark:border-accent  w-full h-fit  py-12 sm:px-2 rounded-lg">
           <div className="absolute top-4 right-4">
             <CloseModal />
