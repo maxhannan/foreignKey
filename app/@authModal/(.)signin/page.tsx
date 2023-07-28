4c22b69b4287c9d6c5bbd4c8b234acb40a819cf1
Max <maxhannan@quenelle.app>
2023-07-27 19:17:15 -0500

    fix

--- app/@authModal/(.)signin/page.tsx
+++ app/@authModal/(.)signin/page.tsx
@@ -7,7 +7,7 @@ const page: FC = () => {
   return (
     <div className="fixed inset-0 bg-zinc-900/20 z-10">
       <div className="px-4 sm:container sm:px-0  flex items-center h-full sm:max-w-[500px] w-full sm:mx-auto">
-        <div className="relative bg-white w-full h-fit py-20 sm:px-2 rounded-lg">
+        <div className="relative bg-background dark:border  dark:border-accent  w-full h-fit  py-12 sm:px-2 rounded-lg">
           <div className="absolute top-4 right-4">
             <CloseModal />
           </div>
--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -7,7 +7,7 @@ import GitHubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
 import { prisma } from "@/lib/prisma";
 import sendVerificationRequest from "@/lib/email";
-
+import { nanoid } from "nanoid";
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET!,
   debug: process.env.NODE_ENV === "development",
@@ -58,12 +58,23 @@ export const authOptions: NextAuthOptions = {
         token.id = user!.id;
         return token;
       }
+      if (!dbUser.username) {
+        await prisma.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            username: nanoid(10),
+          },
+        });
+      }
 
       return {
         id: dbUser.id,
         name: dbUser.name,
         email: dbUser.email,
         picture: dbUser.image,
+        username: dbUser.username,
       };
     },
   },
